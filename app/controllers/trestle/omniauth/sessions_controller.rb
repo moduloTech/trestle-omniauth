a5e910ed16d767645f4dcc6c17e61c2d76402fba
Mikhail Varabyou <varaby_m@modulotech.fr>
2024-04-30 11:32:04 +0200

    redirect from login page if already logged in.

--- app/controllers/trestle/omniauth/sessions_controller.rb
+++ app/controllers/trestle/omniauth/sessions_controller.rb
@@ -2,6 +2,7 @@ class Trestle::Omniauth::SessionsController < Trestle::ApplicationController
   layout "trestle/omniauth"
 
   skip_before_action :require_authenticated_user
+  before_action :check_logged_in, only: :new
 
   def new
     @providers = Trestle.config.omniauth.providers.keys
@@ -15,5 +16,11 @@ class Trestle::Omniauth::SessionsController < Trestle::ApplicationController
   def destroy
     logout!
     redirect_to instance_exec(&Trestle.config.omniauth.redirect_on_logout)
+  end
+
+  private
+
+  def check_logged_in
+    redirect_to(instance_exec(&Trestle.config.omniauth.redirect_on_login)) if logged_in?
   end
 end
